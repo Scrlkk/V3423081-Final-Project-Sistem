17969a111e5778e76b886d8b3561baf9066edc87
Scrlkk <shevaazki6@gmail.com>
2024-11-04 23:46:32 +0700

    Final Project w-Rev

--- src/router/index.ts
+++ src/router/index.ts
@@ -47,6 +47,16 @@ const router = createRouter({
       component: About,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return {
+        ...savedPosition,
+        behavior: 'smooth',
+      }
+    } else {
+      return { top: 0, behavior: 'smooth' }
+    }
+  },
 })
 
 export default router
